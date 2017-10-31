8ec6d25ce58786d706613bc610ece79cc900e210
daianemarcolino <daimmattos@gmail.com>
2017-10-31 17:03:25 -0200

    shiny

--- app/server.R
+++ app/server.R
@@ -1,5 +1,5 @@
 shinyServer(function(input, output) {
-   
+  
   # list of time series available
   output$series_available <- renderUI({
     selectInput(inputId = "series_available", label = "Time series available:", 
@@ -8,22 +8,6 @@ shinyServer(function(input, output) {
   })
   
   # nowcast
-  
-  output$print <- renderPrint({
-
-    pib <- lag(data_series_available[,"serie22099"],-2)
-    y <- month2qtr(diff(diff(pib,3),12))
-    codes <- paste0("serie", as.numeric(sapply(input$series_available, FUN = function(x) unlist(strsplit(x, "-"))[1]))) 
-    x <- Bpanel(data_series_available[,codes], rep(4,length(codes)), aggregate = F)
-    q <- input$number_error
-    r <- input$number_error
-    p <- input$number_lag
-    now <- nowcast(y,x,q,r,p,method = '2sm')
-    str(now)
-    
-    
-  })
-    
   now <- reactive({
     input$now_button
     isolate({
@@ -33,16 +17,16 @@ shinyServer(function(input, output) {
       x <- Bpanel(data_series_available[,codes], rep(4,length(codes)), aggregate = F)
       q <- input$number_shocks
       r <- input$number_factors
-      p <- 1
+      p <- input$number_lag
       now <- nowcast(y,x,q,r,p,method = '2sm')
       now
     })
   })
-
+  
   # graph 1 - y and yhat
   output$graph1 <- renderPlot({
     if(input$now_button != 0)
-    nowcast.plot(now())
+      nowcast.plot(now())
   })
   
   # graph 2 - factors
@@ -67,6 +51,43 @@ shinyServer(function(input, output) {
   output$graph5 <- renderPlot({
     if(input$now_button != 0)
       nowcast.plot(now(), type = "month_y")
+  })
+  
+  # graph 6 - pib yoy
+  output$graph6 <- renderDygraph({
+    if(input$now_button != 0){
+      
+      pib <- lag(data_series_available[,"serie22099"],-2)
+      pibq<-month2qtr(pib)
+      
+      fit_nivel <- now()$main[,2]+lag(pibq,-4)+lag(pibq,-1)-lag(pibq,-5)
+      k <- data.frame(cbind(now()$main[,3],pibq))
+      
+      for(i in 71:75){
+        k[i,2] <- k[i-1,2] + k[i-4,2] - k[i-5,2] + k[i,1]
+      }
+      prev_nivel <- ts(tail(k[,2],5), end = end(now()$main), freq = 4)
+      
+      x <- cbind(pibq, fit_nivel, prev_nivel)
+      if(input$unit_y == "Level"){
+        x[max(which(is.na(x[,3]))),3] <- x[max(which(is.na(x[,3]))),2]
+      }else{
+        varpibq <- (x[,1]/lag(x[,1],-4)-1)*100
+        varfit <- (x[,2]/lag(x[,1],-4)-1)*100
+        prev <- ts(na.omit(c(x[,1],x[,3])), start = start(x), freq = frequency(x))
+        varprev <- ts(c(tail(na.omit(varfit),1), tail((prev/lag(prev,-4)-1)*100,5)), start = end(na.omit(varfit)), freq = 4)
+        x <- cbind(varpibq, varfit, varprev)
+      }
+      colnames(x) <- c("y", "fit","fcst")
+      
+      dygraph(x) %>%
+        dySeries("y", strokePattern = "dotted", color = "#000000") %>%
+        dySeries("fit", strokeWidth = 2, color = "#3299CC") %>%
+        dySeries("fcst", strokeWidth = 2, color = "#3299CC", strokePattern = "dashed") %>%
+        dyAxis("y", axisLabelWidth = 20) %>%
+        dyRangeSelector(dateWindow = c(tail(as.Date(x),1) - years(5),tail(as.Date(x),1)), strokeColor = "#3299CC", fillColor = "#F0F8FF") %>%
+        dyLegend(labelsDiv = "legenda_graph6", show = "always", labelsSeparateLines = T)
+    }
   })
   
   
--- app/ui.R
+++ app/ui.R
@@ -1,8 +1,26 @@
 shinyUI(
   navbarPage(div("Nowcasting", style = "color:white; font-weight:bold; font-family:helvetica"), theme = shinytheme("yeti"), inverse = T,
-             tabPanel("CRAN",""),
-                      
-              tabPanel("Just do it!",
+             # PACKAGE ------------------------------------------------
+             tabPanel("Package", 
+                      wellPanel(style = "background-color:white",
+                                div(#style = "height:280px", 
+                                  div("nowcasting: Nowcast Analysis and Create Real-Time Data Basis", style = "font-family:lucida console;"),
+                                  br(),
+                                  div(icon("angle-right"), "CRAN", style = "font-weight:bold"),
+                                  a(href = "https://CRAN.R-project.org/package=nowcasting", "https://CRAN.R-project.org/package=nowcasting", target = "_blank"), br(), 
+                                  div("install.packages('nowcasting')", style = "background-color:#F0F8FF; height:45px; margin-top:5px; padding-top:15px; padding-left:15px; font-family:lucida console; font-size:90%;border-radius:5px"),
+                                  
+                                  br(), br(),
+                                  div(icon("angle-right"), "Github", style = "font-weight:bold"),
+                                  a(href = "https://github.com/guilbran/nowcasting", "https://github.com/guilbran/nowcasting", target = "_blank"),
+                                  div("devtools::install_github('guilbran/nowcasting')", style = "background-color:#F0F8FF; height:45px; margin-top:5px; padding-top:15px; padding-left:15px; font-family:lucida console; font-size:90%;border-radius:5px")
+                                
+                                )
+                      )
+             ),
+             
+             # JUST DO IT ----------------------------------------------
+             tabPanel("Just do it!",
                       
                       fluidRow(
                         column(offset = 1, width = 10,
@@ -13,8 +31,8 @@ shinyUI(
                                               uiOutput("series_available"),
                                               sliderInput(inputId = "number_factors", label = "Number of factors:", min = 1, max = 5, value = 1),
                                               sliderInput(inputId = "number_shocks", label = "Number of common shocks:", min = 1, max = 5, value = 1),
-                                              #sliderInput(inputId = "number_lag", label = "Factor lag:", min = 1, max = 5, value = 1),
-                                             
+                                              sliderInput(inputId = "number_lag", label = "Factor lag:", min = 1, max = 5, value = 1),
+                                              
                                               
                                               div(bsButton(inputId = "now_button", label = "Nowcast!", style = "primary" ), style = "text-align:center")
                                               
@@ -24,42 +42,69 @@ shinyUI(
                                    
                                    wellPanel(style = "background-color:white",
                                              div(style = "height:400px",
+                                                 
+                                                 fluidRow(
+                                                   column(2, radioButtons(inputId = "unit_y", label = "Unit:", choices = c("Level","YoY")),
+                                                          div(textOutput("legenda_graph6"), style = "font-size:80%;")),
+                                                   column(10,
+                                                          
+                                                          dygraphOutput("graph6", width = "100%"))
+                                                 )
+                                             )
+                                   ),
+                                   wellPanel(style = "background-color:white",
+                                             div(style = "height:300px",
                                                  plotOutput("graph1", width = "100%", height = "100%"))
                                    ),
-                                    
-                                   fluidRow(
-                                     column(6, 
-                                            wellPanel(style = "background-color:white",
-                                                      div(style = "height:300px", 
-                                                          plotOutput("graph2", width = "100%", height = "99%"))
-                                            )
-                                     ),
-                                     column(6, 
-                                            wellPanel(style = "background-color:white",
-                                                      div(style = "height:300px",
-                                                          plotOutput("graph3", width = "100%", height = "99%"))
-                                            )
-                                     )
+                                   
+                                   
+                                   wellPanel(style = "background-color:white",
+                                             div(style = "height:300px", 
+                                                 plotOutput("graph2", width = "100%", height = "99%"))
                                    ),
-                                   fluidRow(
-                                     column(6, 
-                                            wellPanel(style = "background-color:white",
-                                                      div(style = "height:300px", 
-                                                          plotOutput("graph4", width = "100%", height = "99%"))
-                                            )
-                                     ),
-                                     column(6, 
-                                            wellPanel(style = "background-color:white",
-                                                      div(style = "height:300px",
-                                                          plotOutput("graph5", width = "100%", height = "99%"))
-                                            )
-                                     )
+                                   wellPanel(style = "background-color:white",
+                                             div(style = "height:300px",
+                                                 plotOutput("graph3", width = "100%", height = "99%"))
+                                   ),
+                                   wellPanel(style = "background-color:white",
+                                             div(style = "height:300px", 
+                                                 plotOutput("graph4", width = "100%", height = "99%"))
+                                   ),
+                                   wellPanel(style = "background-color:white",
+                                             div(style = "height:300px",
+                                                 plotOutput("graph5", width = "100%", height = "99%"))
                                    )
-                                 )
-                               ))
+                                 ) # main panel
+                               ) # sidebarlayout
+                        )
                       )
-                      
-                      
-             )
-  )
-)
+             ), # tabPanel Just do it!
+             # AUTHORS ---------------------------------------------
+             tabPanel("Authors", 
+                      wellPanel(style = "background-color:white",
+                                div(style = "height:280px",
+                                    icon("angle-right"),"Pedro G. C. Ferreira", br(),
+                                    "PhD. in Electrical Engineering - Methods of Decision Support (PUC-Rio) and master in Economics (UFJF-MG).", br(),
+                                    "Coordenador do NMEC", br(),
+                                    a(href = "mailto:pedro.guilherme@fgv.br","email", title = "pedro.guilherme@fgv.br", style ="color:#3299CC"), "|",
+                                    a(href = "http://lattes.cnpq.br/2228133411590933","lattes", target = "_blank", style = "color:#3299CC"),
+                                    br(),br(),
+                                    
+                                    icon("angle-right"), "Daiane Marcolino de Mattos", br(),
+                                    "Graduate student in Electrical Engineering - Methods of Decision Support (PUC-Rio) and major in Statistical Science (ENCE/IBGE).", br(),
+                                    a(href = "mailto:daiane.mattos@fgv.br","email", title = "daiane.mattos@fgv.br", style ="color:#3299CC"), "|",
+                                    a(href = "http://lattes.cnpq.br/5903694854336540","lattes", target = "_blank", style = "color:#3299CC"),
+                                    br(),br(),
+                                    
+                                    icon("angle-right"),"Guilherme Branco Gomes", br(),
+                                    "Graduate student in Economics (FGV EPGE) and major in Economics (FEA/USP)", br(),
+                                    a(href = "mailto:guilherme.branco@fgv.br","email", title = "guilherme.branco@fgv.br", style ="color:#3299CC"), "|",
+                                    a(href = "http://lattes.cnpq.br/4872383719355805","lattes", target = "_blank", style = "color:#3299CC")
+                                    
+                                    
+                                )
+                                
+                      )
+             ) # tabPanel Authors
+  ) # navbarPage
+) # shinyUI
